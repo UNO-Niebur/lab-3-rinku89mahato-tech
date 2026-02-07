05a0b79c05a996b5926827a94aa4333204b45a5c
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-02-07 04:23:13 +0000

    Completed RPS.py_Lab3

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name:Rinku Mahato
+#Date:02/06/2026
+#Assignment: Play Rock, Paper, Scissor game as long as the user wants to play.
 import random
 
 def main():
@@ -23,16 +23,16 @@ def main():
     if computer == "R":
       print ("Computer Chose Rock")
     elif computer == "P":
-      print ("computer choose paper")
+      print ("computer choose Paper")
     else:
-      print ("computer choose scissors") 
+      print ("computer choose Scissors") 
 
     if player == "R" :
-      print ("Player choose rock")
+      print ("Player choose Rock")
     elif player == "P" :
-      print ("Player choose paper")
+      print ("Player choose Paper")
     else:
-      print ("Player choose scissors")
+      print ("Player choose Scissors")
 
     if player == "R" and computer == "R":
       print ("Tie")
@@ -44,12 +44,29 @@ def main():
       print("You Win!")
       wins = wins + 1
 
+    if player == "P" and computer == "P":
+      print ("Tie")
+      ties = ties + 1
+    if player == "P" and computer == "S":
+      print("Computer Wins.")
+      losses = losses +1
+    if player == "P" and computer == "R":
+      print("You Win!")
+      wins = wins + 1
 
+    if player == "S" and computer == "S":
+      print ("Tie")
+      ties = ties + 1
+    if player == "S" and computer == "R":
+      print("Computer Wins.")
+      losses = losses +1
+    if player == "S" and computer == "P":
+      print("You Win!")
+      wins = wins + 1
+    
   #Ask the user if they would like to play again.
     playAgain = input("Play again? (Y/N): ")
   #play_again =input("would you like to play again" : {yes/No})
-
-
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
