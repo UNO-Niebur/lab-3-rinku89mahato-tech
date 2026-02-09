64c1c6e84ede03e917e63833dd8400440588a260
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-02-09 04:50:18 +0000

    Completed_optional_approxpi.py

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: Rinku Mahato
+#Date: 02/08/2026
+#Assignment: calculate pi using the approximation technique.
 import math
 import time
 
@@ -12,10 +12,21 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  decimal_precision = int(input(" set decimal precision (0-10): "))
+ 
+  
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxpi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxpi, decimal_precision) != round(realPi, decimal_precision):
+    print(approxpi)
+    approxpi = approxpi + (sign * 4 / denom)
+
+    sign = sign * -1
+    denom = denom + 2
 
   end = time.time()
 
